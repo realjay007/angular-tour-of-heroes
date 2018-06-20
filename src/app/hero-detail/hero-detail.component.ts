f8d71f1d266ec24a8d037aaaea632aa6cfc7a430
Julius Ijie <juliusijie@gmail.com>
2018-06-20 17:37:59 +0100

    Actual chapter 5

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,24 +1,46 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
+import { Location } from "@angular/common";
+
 import { Hero } from "../hero";
 import { HEROES } from "../mock-heroes";
+import { HeroService } from "../hero.service";
 
 @Component({
-  selector: 'app-hero-detail',
-  templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+	selector: 'app-hero-detail',
+	templateUrl: './hero-detail.component.html',
+	styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
 
-  @Input() hero: Hero;
+	@Input() hero: Hero;
 
-  constructor() { }
+	constructor(
+		private route: ActivatedRoute,
+		private heroService: HeroService,
+		private location: Location,
+		private router: Router
+	) { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.getHero();
+	}
 
-  randomHero() {
-    let randHero = HEROES[Math.floor(Math.random()*HEROES.length)];
-    this.hero = randHero;
-  }
+	getHero(): void {
+		const id = Number(this.route.snapshot.paramMap.get("id"));
+		this.heroService.getHero(id)
+		.subscribe(hero => this.hero = hero);
+	}
+
+	randomHero() {
+		let randHero = HEROES[Math.floor(Math.random()*HEROES.length)];
+		this.router.navigate(["/detail", randHero.id]);
+		this.hero = randHero;
+		//this.router.navigate(["/dashboard"]);
+	}
+
+	goBack(): void {
+		this.location.back();
+	}
 
 }
